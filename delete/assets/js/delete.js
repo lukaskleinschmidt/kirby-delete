b70287823e442bb19ce85b7823ac1c1dc4c62957
Lukas Kleinschmidt <l.kleinschmidt@crolla-lowis.de>
2017-02-02 14:16:22 +0100

    Added delete action for context menus

--- delete/assets/js/delete.js
+++ delete/assets/js/delete.js
@@ -1,20 +1,34 @@
 (function($) {
 
-  $.fn._sidebar  = $.fn.sidebar;
+  var position = 'sidebar', element = null, clientX, clientY;
 
-  $.fn.sidebar = function() {
+  $(document).ajaxStop(function() {
     add();
-    return this._sidebar();
-  };
+  }).ready(function() {
+    add();
+  }).on('click', '[data-context]', function(e) {
+    position ='context';
+    element  = $(this);
+    clientX  = e.clientX;
+    clientY  = e.clientY;
+  });
 
   var add = function() {
-    var url = window.location.href.replace('/edit', '/options/delete');
+    // Abort when there is already a delete button in the sidebar
+    // But continue when the call comes from a context menu
+    if(position == 'sidebar' && ((window.location.pathname.match(/\//g) || []).length < 3 || $('[data-shortcut="#"]').length)) return;
 
-    if((window.location.pathname.match(/\//g) || []).length < 3 || $('[data-shortcut="#"]').length) return;
+    var url = window.location.href.replace('/edit', '/options/delete/' + position);
+
+    if(position == 'context') {
+      url = element.data('context').replace('/context', '/options/delete/context');
+    }
 
     $.get(url, function(data) {
+
       if(!data) return;
 
+      var target = $('.sidebar-list:nth-of-type(1)');
       var link = $('<a><i class="icon icon-left fa fa-trash-o"></i>' + data.label + '</a>');
 
       if(data.href) {
@@ -33,7 +47,40 @@
         link.attr('data-modal', '');
       }
 
-      $('.sidebar-list:nth-of-type(1)').append($('<li>').append(link))
+      if(position == 'context') {
+        target = $('nav.contextmenu .dropdown-list');
+      }
+
+      // Add link
+      target.append($('<li>').append(link));
+
+      // Reposition if necessary
+      if(position == 'context') {
+        var menu    = $('nav.contextmenu');
+        var top     = clientY;
+        var left    = clientX;
+        var width   = menu.innerWidth();
+        var height  = menu.innerHeight();
+        var wwidth  = $(window).width();
+        var wheight = $(window).height();
+
+        if((left + width) > wwidth) {
+          left = wwidth - width;
+        }
+
+        if((top + height) > wheight) {
+          top = wheight - height;
+        }
+
+        menu.css({
+          top: top,
+          left: left
+        });
+      }
+
+      // Reset position
+      position ='sidebar';
+
     });
   }
 
